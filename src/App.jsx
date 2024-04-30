420b1b3341001e13b16656e6802202c7b5d0a435
SomSingh23 <githubsom@gmail.com>
2024-04-30 08:35:01 +0530

    application zip added, users can download it..

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
-function App() {
-  return <h1>SurakshaScan Landing Page</h1>;
-}
+import saveAs from "file-saver";
+
+const App = () => {
+  const downloadApplication = () => {
+    // Replace 'your_application.zip' with the actual filename of your zip
+    fetch("/btp-6-application.zip")
+      .then((response) => response.blob())
+      .then((blob) => {
+        saveAs(blob, "btp-6-application.zip"); // Set desired download filename here
+      });
+  };
+
+  return (
+    <>
+      <h1>SurakshaScan</h1>
+      <div>
+        <p>Download our application</p>
+        <button onClick={downloadApplication}>Download Application</button>
+      </div>
+    </>
+  );
+};
 
 export default App;
